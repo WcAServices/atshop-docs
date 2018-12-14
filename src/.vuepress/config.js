6e1a5446bf7d48c4c7a810720d2690d6787572bc
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-14 15:43:44 +0200

    Add back-to-ATShop link to navbar.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,13 @@
 module.exports = {
     title: 'ATShop API',
     description: 'API documentation for ATShop.io',
+
+    themeConfig: {
+        nav: [
+            {
+                text: 'ATShop',
+                link: 'https://atshop.io/'
+            }
+        ]
+    }
 };

ecc1c2929ae5dd80d52c8e155fe7c7ee355d116b
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-14 16:59:46 +0200

    Update DDP Setup example.

--- snippets/node/establish-ddp.js
+++ snippets/node/establish-ddp.js
@@ -1,13 +1,16 @@
 const DDP = require('ddp');
 
+/* Prepare the client */
 const client = new DDP({
     url: 'wss://atshop.io/websocket',
 });
 
+/* Connect to the server */
 client.connect((err, reconnected) => {
     if (err) {
         throw err;
     }
 
     // Successfully connected!
+
 });
--- src/guide/getting-started.md
+++ src/guide/getting-started.md
@@ -1,4 +1,5 @@
 # Setting up DDP
 Getting hooked into the API is fairly straightforward:
 
+#### Set up and connect to DDP
 <<< ../snippets/node/establish-ddp.js

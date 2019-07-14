160c7e7a735f21247093dcc42d69cec5cbd7f15e
Jørgen Vatle <Jorgen@WcAServices.net>
2019-07-14 18:28:03 +0300

    Update fetch-ordered-product example content.

--- snippets/node/orders/publications/fetch-ordered-product.js
+++ snippets/node/orders/publications/fetch-ordered-product.js
@@ -1,7 +1,17 @@
-const orderId = 'your-order-id-here';
+const order = { // Order from the 'Orders' publication example.
+    _id: 'order-id-from-example',
+    productId: 'product-id',
+};
 
-DDPClient.subscribe('ordered.product', [orderId], () => {
+DDPClient.subscribe('ordered.product', [order._id], () => {
     const collection = DDPClient.collections['shop.products'];
+    const product = collection[order.productId];
 
-    console.log(collection);
+    product._id         // Product ID
+    product.name        // Product name
+    product.description // Product description as defined in the product manager page.
+    product.minQuantity // Minimum order quantity.
+    product.shopId      // ID of the shop this product belongs to.
+    product.stockCount  // Remaining stock.
+    product.value       // Value in cents for this product. ($1 = 100).
 });

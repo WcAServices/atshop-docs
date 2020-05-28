62766542f2d62b9982bac5e32387bf798ce9a713
Jørgen Vatle <Jorgen@WcAServices.net>
2020-05-28 22:50:14 +0300

    Update wording for productIds prop in edit-product-group.js

--- snippets/node/product-groups/edit-product-group.js
+++ snippets/node/product-groups/edit-product-group.js
@@ -13,7 +13,7 @@ const productGroup = {
     maxDisplayedStock: 0, // Max number of stock to display to your customers for any attached products. Set to '0' to display an accurate representation of available stock.
     category: '', // ID of the category this product group should show up in.
     displayDescription: true, // Whether you want to display the description for this group directly on its card or just in the purchase modal.
-    productIds: ['lqHmT5CuB7Zhigzfz'], // Array of products to attach to this product group.
+    productIds: ['lqHmT5CuB7Zhigzfz'], // Array of product IDs to attach to this product group.
 }
 
 DDPClient.call('shop.product.groups.save', [productGroup], (err, resp) => {

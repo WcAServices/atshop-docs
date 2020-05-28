665140916b15cd8009ebec2970de4eff0a3f739e
Jørgen Vatle <Jorgen@WcAServices.net>
2020-05-28 13:46:59 +0300

    Add product group creation example.

---
+++ docs/reference/methods/product-groups.md
@@ -0,0 +1,4 @@
+# Product Group
+
+## Create a new group
+<<< ../snippets/node/product-groups/create-product-group.js
---
+++ snippets/node/product-groups/create-product-group.js
@@ -0,0 +1,23 @@
+const shopId = 'your-shop-ID';
+
+const productGroup = {
+    shopId: shopId, // Your Shop ID
+    name: 'My Product Group',
+    description: '', // Markdown supported.
+    style: 'card', // Product card style. Can be 'dynamic', 'box', or 'card'.
+    image_url: '', // URI to product group image.
+    icon: '', // Icon from https://materialdesignicons.com/ We're running a lightweight version so some icons may not work.
+    priority: 15, // Display priority for this group. Lower numbers will result in the product showing up earlier in the list of product. Higher numbers will show up later.
+    maxDisplayedStock: 0, // Max number of stock to display to your customers for any attached products. Set to '0' to display an accurate representation of available stock.
+    category: '', // ID of the category this product group should show up in.
+    displayDescription: true, // Whether you want to display the description for this group directly on its card or just in the purchase modal.
+    productIds: ['lqHmT5CuB7Zhigzfz'], // Array of products to attach to this product group.
+}
+
+DDPClient.call('shop.product.groups.save', [productGroup], (err, resp) => {
+    if (err) {
+        return console.error(err);
+    }
+
+    console.log('Successfully created product group!');
+})

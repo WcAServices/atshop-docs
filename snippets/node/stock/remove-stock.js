84d3f42450b7b189763853f1961428b1850a83ad
Jørgen Vatle <Jorgen@WcAServices.net>
2020-06-16 01:13:11 +0300

    Indicate that stock removal can be batched.

--- snippets/node/stock/remove-stock.js
+++ snippets/node/stock/remove-stock.js
@@ -3,6 +3,12 @@ const stockToRemove = [
         _id: '_id of the stock entry you want to remove',
         productId: '_id of the product this stock entry belongs to',
     },
+    {
+        _id: '_id of another stock entry',
+        productId: 'associated product ID for this stock entry'
+    },
+    /// ...
+    // Feel free to populate with as many or as few stock entries as you wish to delete at once.
 ]
 
 DDPClient.call('admin.product.stock.delete', [stockToRemove], (err) => {
